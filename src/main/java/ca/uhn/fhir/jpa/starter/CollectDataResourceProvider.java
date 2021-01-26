ed8aa26ed175a8f95fe6dfd6c091aa9f9453040e
mozzy11 <mozzymutesa@gmail.com>
2021-01-26 17:21:47 +0300

    clean up

--- src/main/java/ca/uhn/fhir/jpa/starter/CollectDataResourceProvider.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/CollectDataResourceProvider.java
@@ -1,32 +1,35 @@
 package ca.uhn.fhir.jpa.starter;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
+import com.google.common.base.Charsets;
+
 import org.apache.commons.codec.binary.Base64;
-import org.apache.http.HttpEntity;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.Measure;
 import org.hl7.fhir.r4.model.MeasureReport;
 import org.hl7.fhir.r4.model.Parameters;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.jpa.rp.r4.MeasureResourceProvider;
 import ca.uhn.fhir.jpa.rp.r4.ObservationResourceProvider;
 import ca.uhn.fhir.rest.annotation.IdParam;
 import ca.uhn.fhir.rest.annotation.Operation;
 import ca.uhn.fhir.rest.annotation.OperationParam;
 import ca.uhn.fhir.rest.api.Constants;
-import org.apache.http.impl.client.HttpClientBuilder;
+import ca.uhn.fhir.rest.api.EncodingEnum;
 
 public class CollectDataResourceProvider {
     protected static CloseableHttpClient ourHttpClient;
@@ -37,64 +40,43 @@ public class CollectDataResourceProvider {
     @Autowired
     ObservationResourceProvider ObservationResourceProvider;
 
+    FhirContext ctx = FhirContext.forR4();
+
+    private String fhirBaseUrl;
+
     @Operation(name = PlirConstants.OPERATION_COLLECT_DATA, idempotent = true, type = Measure.class)
-    public Parameters collectDataOperation(
-            HttpServletRequest theServletRequest,
-            @IdParam IdType theId,
+    public Parameters collectDataOperation(HttpServletRequest theServletRequest, @IdParam IdType theId,
             @OperationParam(name = PlirConstants.PARAM_PERIOD_START, min = 1, max = 1) String periodStart,
-            @OperationParam(name = PlirConstants.PARAM_PERIOD_END, min = 1, max = 1) String periodEnd) {
+            @OperationParam(name = PlirConstants.PARAM_PERIOD_END, min = 1, max = 1) String periodEnd)
+            throws ClientProtocolException, IOException {
 
-        Parameters  parameters = new Parameters();
-
-        MeasureReport report = evaluateMeasure(theId, periodStart ,periodEnd);
+        Parameters parameters = new Parameters();
 
         Measure measure = this.measureResourceProvider.getDao().read(theId);
+        MeasureReport report = evaluateMeasure(measure, periodStart, periodEnd);
 
+        parameters.addParameter(new Parameters.ParametersParameterComponent()
+                .setName(PlirConstants.RESOURCE_MEASURE_REPORT).setResource(report));
 
-        parameters.addParameter(
-            new Parameters.ParametersParameterComponent().setName(PlirConstants.RESOURCE_MEASURE_REPORT).setResource(report));
+        System.out.println(">>>>>>>>>>***************ya we made it man*****************<<<<<<<<<<<<<<<<");
+        fhirBaseUrl = theServletRequest.getContextPath();
 
-      System.out.println(">>>>>>>>>>***************ya we made it man*****************<<<<<<<<<<<<<<<<");
-        String baseUrl = theServletRequest.getContextPath() ;
-        System.out.println("Base Url :" + baseUrl);
-
-        String servletPath = theServletRequest.getServletPath();
-
-        System.out.println("servletPath :" + servletPath);
-
-        ServletContext serv = theServletRequest.getServletContext();
-
-        String contextPath = serv.getContextPath();
-
-        System.out.println("contextPath :" + contextPath);
-        
-        String url = theServletRequest.getRequestURI() ;
-
-        System.out.println("whole Url :" + url);
-        String params = theServletRequest.getQueryString() ;
-
-        System.out.println("query String :" + params);
-       System.out.println(">>>>>>>>>>***************my Measure id*****************<<<<<<<<<<<<<<<<");   
-
-        // IIdType theId //
         System.out.println(">>>>>>>>>>***************ya we made it man*****************<<<<<<<<<<<<<<<<");
         System.out.println("measure : " + measure.getIdentifier().iterator().next().getValue());
 
         System.out.println("periodStart :" + periodStart);
-        // System.out.println(">>>>>>>>>>***************my Measure id*****************<<<<<<<<<<<<<<<<");
-        return  parameters;
+        // System.out.println(">>>>>>>>>>***************my Measure
+        // id*****************<<<<<<<<<<<<<<<<");
+        return parameters;
     }
 
-    public MeasureReport evaluateMeasure(@IdParam IdType theId,
-            @OperationParam(name = "periodStart") String periodStart,
-            @OperationParam(name = "periodEnd") String periodEnd) {
+    public MeasureReport evaluateMeasure(Measure measure, String periodStart, String periosEnd)
+            throws ClientProtocolException, IOException {
 
-        Measure measure = this.measureResourceProvider.getDao().read(theId);
         String expression = measure.getGroupFirstRep().getPopulationFirstRep().getCriteria().getExpression();
 
-
         if (measure == null) {
-            throw new RuntimeException("Could not find Measure/" + theId.getIdPart());
+            throw new RuntimeException("Could not find Measure/" + measure.getId());
         }
 
         MeasureReport report = new MeasureReport();
@@ -102,35 +84,36 @@ public class CollectDataResourceProvider {
         report.setType(MeasureReport.MeasureReportType.DATACOLLECTION);
         report.setMeasure(PlirConstants.MEASURE_RESOURCE_MEASURE);
 
-        //Bundle bundle  = ObservationResourceProvider.getDao().search(theParams);
-        //report.setEvaluatedResource(theEvaluatedResource);
+        Bundle bundle = fetchBundle(fhirBaseUrl + "/fhir/" + expression);
+        // report.setEvaluatedResource(theEvaluatedResource);
         return report;
 
     }
 
+    private Bundle fetchBundle(String theUrl) throws IOException, ClientProtocolException {
 
-    private String fetchREsp(String theUrl) throws IOException, ClientProtocolException {
-
+        Bundle bundle;
         ourHttpClient = HttpClientBuilder.create().build();
         HttpGet get = new HttpGet(theUrl);
-    
+
         String username = "hapi";
         String password = "hapi123";
-    
+
         String auth = username + ":" + password;
         byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(StandardCharsets.ISO_8859_1));
-         String authHeader = "Basic " + new String(encodedAuth);
-         get.addHeader(HttpHeaders.AUTHORIZATION, authHeader);
-    
+        String authHeader = "Basic " + new String(encodedAuth);
+        get.addHeader(HttpHeaders.AUTHORIZATION, authHeader);
+
         get.addHeader(Constants.HEADER_CACHE_CONTROL, Constants.CACHE_CONTROL_NO_CACHE);
         CloseableHttpResponse resp = ourHttpClient.execute(get);
-        
-        HttpEntity responseEntity = resp.getEntity();
-            ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-            responseEntity.writeTo(byteStream);
-            
-        return byteStream.toString();
-        //return resp.getStatusLine().toString();
+
+        try {
+            bundle = EncodingEnum.JSON.newParser(ctx).parseResource(Bundle.class,
+                    IOUtils.toString(resp.getEntity().getContent(), Charsets.UTF_8));
+        } finally {
+            IOUtils.closeQuietly(resp);
         }
+        return bundle;
+    }
 
 }
--- src/test/java/ca/uhn/fhir/jpa/starter/OperationClientTest.java
+++ src/test/java/ca/uhn/fhir/jpa/starter/OperationClientTest.java
@@ -1,221 +1,189 @@
 package ca.uhn.fhir.jpa.starter;
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.model.primitive.DateTimeDt;
-import ca.uhn.fhir.parser.StrictErrorHandler;
-import ca.uhn.fhir.rest.api.CacheControlDirective;
-import ca.uhn.fhir.rest.api.Constants;
-import ca.uhn.fhir.rest.api.EncodingEnum;
-import ca.uhn.fhir.rest.api.MethodOutcome;
-import ca.uhn.fhir.rest.client.api.IClientInterceptor;
-import ca.uhn.fhir.rest.client.api.IGenericClient;
-import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
-import ca.uhn.fhir.rest.client.interceptor.BasicAuthInterceptor;
-import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
-import ca.uhn.fhir.rest.param.DateAndListParam;
-import ca.uhn.fhir.util.BundleUtil;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.IOUtils;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 import com.google.common.base.Charsets;
 
-import org.apache.http.HttpEntity;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpHeaders;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.eclipse.jetty.websocket.api.Session;
-import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
-import org.eclipse.jetty.websocket.client.WebSocketClient;
-import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.Measure;
 import org.hl7.fhir.r4.model.Parameters;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-
-import static ca.uhn.fhir.util.TestUtil.waitForSize;
-import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.parser.StrictErrorHandler;
+import ca.uhn.fhir.rest.api.Constants;
+import ca.uhn.fhir.rest.api.EncodingEnum;
+import ca.uhn.fhir.rest.client.api.IClientInterceptor;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
+import ca.uhn.fhir.rest.client.interceptor.BasicAuthInterceptor;
+import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class, properties =
-  {
-    "spring.batch.job.enabled=false",
-    "spring.datasource.url=jdbc:h2:mem:dbr4",
-    "hapi.fhir.fhir_version=r4",
-    "hapi.fhir.subscription.websocket_enabled=true",
-    "hapi.fhir.empi_enabled=true",
-    //Override is currently required when using Empi as the construction of the Empi beans are ambiguous as they are constructed multiple places. This is evident when running in a spring boot environment
-    "spring.main.allow-bean-definition-overriding=true"
-  })
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class, properties = {
+    "spring.batch.job.enabled=false", "spring.datasource.url=jdbc:h2:mem:dbr4", "hapi.fhir.fhir_version=r4",
+    "hapi.fhir.subscription.websocket_enabled=true", "hapi.fhir.empi_enabled=true",
+    // Override is currently required when using Empi as the construction of the
+    // Empi beans are ambiguous as they are constructed multiple places. This is
+    // evident when running in a spring boot environment
+    "spring.main.allow-bean-definition-overriding=true" })
 public class OperationClientTest {
 
-    private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(ExampleServerDstu2IT.class);
-    private IGenericClient ourClient;
-    private FhirContext ourCtx;
-    protected static CloseableHttpClient ourHttpClient;
-    protected static String ourServerBase;
-    String ourServerBase2 ;
+  private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(ExampleServerDstu2IT.class);
+  private IGenericClient ourClient;
+  private FhirContext ourCtx;
+  protected static CloseableHttpClient ourHttpClient;
+  protected static String ourServerBase;
 
-    // @Autowired
-    // protected FhirContext myFhirCtx;
-  
-    @LocalServerPort
-    private int port;
+  private static String OBS_FILE_PATH = "src/test/resources/ObsBundle.json";
 
-    @Test
-    public void testOperation () throws ClientProtocolException, IOException {
+  private static String PATIENT_FILE_PATH = "src/test/resources/PatientBundle.json";
 
-      Measure measure = new Measure();
-      measure.setId("TX-PVLS");
-      measure.addIdentifier().setValue("Measure TX-PVLS");
-      IIdType id =ourClient.update().resource(measure).withId("TX-PVLS").execute().getId();
-      // System.out.println(">>>>>>>>>>>*****************finally  aaaaaaa****************<<<<<<<<<<<");
-      // System.out.println(id.getValue());
-      // System.out.println(">>>>>>>>>>>*********************************<<<<<<<<<<<");
+  private static String MEASURE_FILE_PATH = "src/test/resources/FhirMeasure.json";
 
+  private static String MEASURE_RESOURCE_ID = "TX-PVLS";
 
-    
+  @LocalServerPort
+  private int port;
 
-    
-      //Parameters params = fetchParameter(ourServerBase + "$collect-data?periodStart=2020-01-01&periodEnd=2020-12-31", EncodingEnum.JSON);
-      
-      String resp = fetchREsp(ourServerBase + "Measure" + "/TX-PVLS/$collect-data?periodStart=2020-01-01&periodEnd=2020-12-31");
+  @Test
+  public void testCollectOperation() throws ClientProtocolException, IOException {
 
-    
-      //String resp = fetchREsp(ourServerBase + "Measure/TX-PVLS/$collect-data?periodStart=2020-01-01&periodEnd=2020-12-31");
-      //  System.out.println(">>>>>>>>>>>*********************************<<<<<<<<<<<");
-      // System.out.println(resp);
-      // System.out.println(">>>>>>>>>>>*********************************<<<<<<<<<<<");
+    // Post the Measure Resource
 
-      // Parameters respParams = ourClient
-			// .operation()
-      // .onType(Measure.class)
-      // .named(PlirConstants.OPERATION_COLLECT_DATA)
-      // .withNoParameters(Parameters.class)
-      // .execute();
-      
-      // Measure measure = fetchMeasure("http://localhost:8888/fhir/Measure/TX-PVLS", EncodingEnum.JSON);
-    
-      // String expression = measure.getGroupFirstRep().getPopulationFirstRep().getCriteria().getExpression();
+    Measure measure = readMeasureFromFile();
+    ourClient.update().resource(measure).withId(MEASURE_RESOURCE_ID).encodedJson().execute();
 
-      // System.out.println(">>>>>>>>>>>*********************************<<<<<<<<<<<");
-      // System.out.println(expression);
-      // System.out.println(">>>>>>>>>>>*********************************<<<<<<<<<<<");
-    }
+    // post the patient BUndle
+    postResource(ourServerBase, PATIENT_FILE_PATH);
 
-    @BeforeEach
-    void beforeEach() {
-  
-      ourCtx = FhirContext.forR4();
-      ourCtx.getRestfulClientFactory().setServerValidationMode(ServerValidationModeEnum.NEVER);
-      ourCtx.getRestfulClientFactory().setSocketTimeout(1200 * 1000);
-      ourCtx.setParserErrorHandler(new StrictErrorHandler());
-      
-      ourServerBase = "http://localhost:" + port + "/fhir/";
-      ourClient = ourCtx.newRestfulGenericClient(ourServerBase);
-      ourClient.registerInterceptor(new LoggingInterceptor(true));
+    // post obs BUndle
+    postResource(ourServerBase, OBS_FILE_PATH);
 
-      //ourServerBase2 = "http://localhost:" + port + "/fhir/context/";
-      
-      //Create an HTTP basic auth interceptor
-      String username = "hapi";
-      String password = "hapi123";
-      IClientInterceptor authInterceptor = new BasicAuthInterceptor(username, password);
-      ourClient.registerInterceptor(authInterceptor); 
+    // fetch parameter
+    Parameters params = fetchParameter(ourServerBase + "$collect-data?periodStart=2020-01-01&periodEnd=2020-12-31");
 
-      ourHttpClient = HttpClientBuilder.create().build();
-    }
+    // System.out.println(">>>>>>>>>>>*********************************<<<<<<<<<<<");
+    // System.out.println(expression);
+    // System.out.println(">>>>>>>>>>>*********************************<<<<<<<<<<<");
+  }
 
-    private Parameters fetchParameter(String theUrl, EncodingEnum theEncoding) throws IOException, ClientProtocolException {
+  @BeforeEach
+  void beforeEach() {
+    ourServerBase = "http://localhost:" + port + "/fhir/";
+    setHapiClient();
+    ourHttpClient = HttpClientBuilder.create().build();
+
+  }
+
+  private Parameters fetchParameter(String theUrl) throws IOException, ClientProtocolException {
     Parameters parameters;
     HttpGet get = new HttpGet(theUrl);
-    
-    
-
-		CloseableHttpResponse resp = ourHttpClient.execute(get);
-		try {
-			assertEquals(theEncoding.getResourceContentTypeNonLegacy(), resp.getFirstHeader(ca.uhn.fhir.rest.api.Constants.HEADER_CONTENT_TYPE).getValue().replaceAll(";.*", ""));
-      parameters = theEncoding.newParser(ourCtx).parseResource(Parameters.class, IOUtils.toString(resp.getEntity().getContent(), Charsets.UTF_8));
-		} finally {
-			IOUtils.closeQuietly(resp);
-		}
-		
-		return parameters;
-  }
-
-  private Measure fetchMeasure(String theUrl, EncodingEnum theEncoding) throws IOException, ClientProtocolException {
-    Measure measure;
-    HttpGet get = new HttpGet(theUrl);
 
     String username = "hapi";
     String password = "hapi123";
 
     String auth = username + ":" + password;
     byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(StandardCharsets.ISO_8859_1));
-     String authHeader = "Basic " + new String(encodedAuth);
-     get.addHeader(HttpHeaders.AUTHORIZATION, authHeader);
+    String authHeader = "Basic " + new String(encodedAuth);
+    get.addHeader(HttpHeaders.AUTHORIZATION, authHeader);
 
     get.addHeader(Constants.HEADER_CACHE_CONTROL, Constants.CACHE_CONTROL_NO_CACHE);
-    
-    
 
-		CloseableHttpResponse resp = ourHttpClient.execute(get);
-		try {
-			assertEquals(theEncoding.getResourceContentTypeNonLegacy(), resp.getFirstHeader(ca.uhn.fhir.rest.api.Constants.HEADER_CONTENT_TYPE).getValue().replaceAll(";.*", ""));
-      measure = theEncoding.newParser(ourCtx).parseResource(Measure.class, IOUtils.toString(resp.getEntity().getContent(), Charsets.UTF_8));
-		} finally {
-			IOUtils.closeQuietly(resp);
-		}
-		
-		return measure;
-  }
-  
-
-  private String fetchREsp(String theUrl) throws IOException, ClientProtocolException {
-    HttpGet get = new HttpGet(theUrl);
-
-    String username = "hapi";
-    String password = "hapi123";
-
-    String auth = username + ":" + password;
-    byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(StandardCharsets.ISO_8859_1));
-     String authHeader = "Basic " + new String(encodedAuth);
-     get.addHeader(HttpHeaders.AUTHORIZATION, authHeader);
-
-    get.addHeader(Constants.HEADER_CACHE_CONTROL, Constants.CACHE_CONTROL_NO_CACHE);
     CloseableHttpResponse resp = ourHttpClient.execute(get);
-    
-    HttpEntity responseEntity = resp.getEntity();
-		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-		responseEntity.writeTo(byteStream);
-		
-    return byteStream.toString();
-    //return resp.getStatusLine().toString();
-	}
+    try {
+      assertEquals(EncodingEnum.JSON.getResourceContentTypeNonLegacy(),
+          resp.getFirstHeader(ca.uhn.fhir.rest.api.Constants.HEADER_CONTENT_TYPE).getValue().replaceAll(";.*", ""));
+      parameters = EncodingEnum.JSON.newParser(ourCtx).parseResource(Parameters.class,
+          IOUtils.toString(resp.getEntity().getContent(), Charsets.UTF_8));
+    } finally {
+      IOUtils.closeQuietly(resp);
+    }
+    return parameters;
   }
-    
 
+  private CloseableHttpResponse postResource(String theUrl, String path) throws IOException, ClientProtocolException {
+    HttpPost post = new HttpPost(theUrl);
+    String username = "hapi";
+    String password = "hapi123";
+    String json = readJsonFile(path);
+
+    StringEntity entity = new StringEntity(json);
+    post.setEntity(entity);
+    post.setHeader("Accept", "application/json");
+    post.setHeader("Content-type", "application/json");
+
+    String auth = username + ":" + password;
+    byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(StandardCharsets.ISO_8859_1));
+    String authHeader = "Basic " + new String(encodedAuth);
+    post.addHeader(HttpHeaders.AUTHORIZATION, authHeader);
+
+    post.addHeader(Constants.HEADER_CACHE_CONTROL, Constants.CACHE_CONTROL_NO_CACHE);
+    CloseableHttpResponse resp = ourHttpClient.execute(post);
+
+    return resp;
+  }
+
+  private String readJsonFile(String path) throws IOException {
+    String content = null;
+    try {
+      content = FileUtils.readFileToString(new File(path), StandardCharsets.UTF_8);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    return content;
+  }
+
+  private void setHapiClient() {
+    ourCtx = FhirContext.forR4();
+    ourCtx.getRestfulClientFactory().setServerValidationMode(ServerValidationModeEnum.NEVER);
+    ourCtx.getRestfulClientFactory().setSocketTimeout(1200 * 1000);
+    ourCtx.setParserErrorHandler(new StrictErrorHandler());
+
+    ourClient = ourCtx.newRestfulGenericClient(ourServerBase);
+    ourClient.registerInterceptor(new LoggingInterceptor(true));
+
+    // Create an HTTP basic auth interceptor
+    String username = "hapi";
+    String password = "hapi123";
+    IClientInterceptor authInterceptor = new BasicAuthInterceptor(username, password);
+    ourClient.registerInterceptor(authInterceptor);
+  }
+
+  public Measure readMeasureFromFile() throws IOException {
+    String json = readJsonFile(MEASURE_FILE_PATH);
+
+    IParser parser = ourCtx.newJsonParser();
+    Measure measure = parser.parseResource(Measure.class, json);
+    return measure;
+  }
+
+  // HttpEntity responseEntity = resp.getEntity();
+  // ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+  // responseEntity.writeTo(byteStream);
+
+  // return byteStream.toString();
+  // //return resp.getStatusLine().toString();
+
+}
